3e21344e70e7ab44f5cdb5fb9bae192a84fa2e65
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-04-01 13:17:16 -0600

    Initial Commit

---
+++ Poema/PoemaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoemaApp.swift
+//  Poema
+//
+//  Created by Benjamin Rojo on 01/04/25.
+//
+
+import SwiftUI
+
+@main
+struct PoemaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
